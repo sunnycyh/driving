e80f8bcb3c8894ddc77d223cda7a135fe3ecf1ab
Sunny Choi <yukhengchoi@gmail.com>
2020-04-26 01:31:32 +0800

    add else part

--- driving.py
+++ driving.py
@@ -1,13 +1,15 @@
 country = input('Please enter your nationality: ')
 age = input('Please enter your age: ')
 age = int(age)
-if country == 'Taiwan' or country == 'taiwan' or country == 'Hong Kong' or country == 'HK':
+if country == 'Taiwan' or country == 'Hong Kong':
 	if age >= 18:
 		print('You can now apply the driving licences!')
 	else:
 		print('You are not allowed to apply the driving licences!')
-elif country == 'America' or country == 'U.S' or country == 'US':
+elif country == 'America':
 	if age >= 16:
 		print('You can now apply the driving licences!')
 	else:
 		print('You are not allowed to apply the driving licences!')
+else:
+	print('You can only type Taiwan / America / Hong Kong')
