027cae0edeff465c36c0309065f1b9a1dd05738b
Sunny Choi <yukhengchoi@gmail.com>
2020-04-26 01:19:16 +0800

    add Taiwan HK

---
+++ driving.py
@@ -0,0 +1,8 @@
+country = input('Please enter your nationality: ')
+age = input('Please enter your age: ')
+age = int(age)
+if country == 'Taiwan' or country == 'taiwan' or country == 'Hong Kong' or country == 'HK':
+	if age >= 18:
+		print('You can now apply the driving licences!')
+	else:
+		print('You are not allowed to apply the driving licences!')
